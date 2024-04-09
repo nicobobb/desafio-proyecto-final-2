0ace7ca6b4c92806d457382616539e7e3489f9a0
MariIglesia <nicobobb@gmail.com>
2024-04-09 00:55:15 -0300

    add api

---
+++ app/util/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: "https://6613568e53b0d5d80f673015.mockapi.io/api/v1/",
+});
+
+export const getProducts = async () => {
+    const resp = await axiosInstance.get("/products");
+    return resp.data;
+};
+
+export const postMessage = async (body) => {
+    const resp = await axiosInstance.post("/messages", body);
+    return resp.data;
+};
