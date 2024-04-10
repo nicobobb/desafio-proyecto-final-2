166567e688f91e55e8462d5118d3c8ea646a79d1
MariIglesia <nicobobb@gmail.com>
2024-04-10 20:49:52 -0300

    fix header

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import Header from "./components/Header";
 import ProductsWrapper from "./components/ProductsWrapper";
 
 export default function Home() {
     return (
         <>
-            <Header />
             <ProductsWrapper />
         </>
     );
