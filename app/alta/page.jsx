c363e9afe8afa4900a8ad787b5d32c550cb5c271
Nico Bobb <nicobobb@gmail.com>
2024-04-14 11:49:13 -0300

    add api in alta form

--- app/alta/page.jsx
+++ app/alta/page.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+"use client";
+import { useForm } from "react-hook-form";
+import { postProduct } from "../util/api";
 
 const Alta = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        data.img =
+            "https://res.cloudinary.com/dh92rbo5b/image/upload/ar_1:1,c_fill/v1713105598/base-photo_pwoge7.webp";
+        postProduct(data)
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+    };
+
     return (
         <div>
             <div
@@ -11,52 +27,102 @@ const Alta = () => {
                     Alta
                 </h3>
             </div>
-            <form className="m-auto w-11/12 max-w-96 my-6">
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="m-auto w-11/12 max-w-96 my-6"
+            >
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Nombre (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("name", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.name && (
+                    <span className="text-red-500">Complete el Nombre</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Precio (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("price", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.price && (
+                    <span className="text-red-500">Completa el precio</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Stock (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("stock", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.stock && (
+                    <span className="text-red-500">Complete el Stock</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Marca (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("brand", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.brand && (
+                    <span className="text-red-500">Complete la Marca</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Categoría (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("category", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.category && (
+                    <span className="text-red-500">Complete la Categoría</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Descripción corta (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("shortDesc", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.shortDesc && (
+                    <span className="text-red-500">
+                        Complete la descripción corta
+                    </span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Descripción larga (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("longDesc", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.longDesc && (
+                    <span className="text-red-500">
+                        Complete la descripción larga
+                    </span>
+                )}
                 <fieldset className="my-4 pt-px pl-2  ">
                     <input
                         type="checkbox"
+                        {...register("delivery")}
                         className="peer size-3.5 appearance-none rounded-sm border border-slate-300 accent-[#b98503] checked:appearance-auto bg-[#fffbf2] focus:outline-none"
                     />
                     <span className="px-2 text-[#c78f02]">
@@ -67,17 +133,29 @@ const Alta = () => {
                     <legend className="px-2">
                         <span className="px-2">Edad desde (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("ageFrom", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.ageFrom && (
+                    <span className="text-red-500">Complete el campo</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
                         <span className="px-2">Edad hasta (*)</span>
                     </legend>
-                    <input className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none" />
+                    <input
+                        {...register("ageTo", { required: true })}
+                        className="py-1 rounded-sm bg-[#fffbf2] focus:outline-none"
+                    />
                 </fieldset>
+                {errors.ageTo && (
+                    <span className="text-red-500">Complete el campo</span>
+                )}
                 <fieldset className="my-4 pt-px pl-2 bg-[#fffbf2] text-[#c78f02] border rounded outline-none border-[#c78f02]">
                     <legend className="px-2">
-                        <span className="px-2">Foto (*)</span>
+                        <span className="px-2">Foto</span>
                     </legend>
                     <input
                         type="file"
@@ -85,7 +163,10 @@ const Alta = () => {
                     />
                 </fieldset>
                 <div className="text-center">
-                    <button className="py-2 px-4 border border-[#b98503] rounded shadow-md bg-[#e7d6ac] hover:bg-[#f3ead5] text-[#b98503]">
+                    <button
+                        type="submit"
+                        className="py-2 px-4 border border-[#b98503] rounded shadow-md bg-[#e7d6ac] hover:bg-[#f3ead5] text-[#b98503]"
+                    >
                         Cargar producto
                     </button>
                 </div>
